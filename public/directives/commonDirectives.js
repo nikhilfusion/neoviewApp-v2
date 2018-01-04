a36fa143788335a348479f78ff112f0f9f2c76f0
Nikhil T Nair <nikhiltnair@outlook.com>
2018-01-05 03:30:39 +0530

    resolve bugs

--- public/directives/commonDirectives.js
+++ public/directives/commonDirectives.js
@@ -1,34 +1,18 @@
 angular.module('neoviewApp')
-.directive("passwordVerify", function() {
+.directive('pwCheck', [function () {
   return {
-    require: "ngModel",
-    scope: {
-      passwordVerify: '='
-    },
-    link: function(scope, element, attrs, ctrl) {
-      scope.$watch(function() {
-        var combined;
-        if (scope.passwordVerify || ctrl.$viewValue) {
-          combined = scope.passwordVerify + '_' + ctrl.$viewValue; 
-        }                    
-        return combined;
-      }, function(value) {
-        if (value) {
-          ctrl.$parsers.unshift(function(viewValue) {
-            var origin = scope.passwordVerify;
-            if (origin !== viewValue) {
-              ctrl.$setValidity("passwordVerify", false);
-              return undefined;
-            } else {
-              ctrl.$setValidity("passwordVerify", true);
-              return viewValue;
-            }
-          });
-        }
+    require: 'ngModel',
+    link: function (scope, elem, attrs, ctrl) {
+      var firstPassword = '#' + attrs.pwCheck;
+      elem.add(firstPassword).on('keyup', function () {
+        scope.$apply(function () {
+          var v = elem.val()===$(firstPassword).val();
+          ctrl.$setValidity('pwmatch', v);
+        });
       });
     }
-  };
-})
+  }
+}])
 .directive('focus',
   function($timeout) {
    return {
