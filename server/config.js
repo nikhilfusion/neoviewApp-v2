114531f86e42ad6d9bb873cbe9ea7ce9d392eb80
Nikhil T Nair <nikhiltnair@outlook.com>
2018-04-24 09:44:19 +0530

    changed email

--- server/config.js
+++ server/config.js
@@ -8,8 +8,8 @@ module.exports = {
     port: "9080",
     apiHost: "192.168.0.151",
     apiPort: "8080",
-    gmailUser: "niktestplancess@gmail.com",
-    gmailPswd: "nikhil123",
+    gmailUser: "neoviewIreland@gmail.com",
+    gmailPswd: "neoview1234@#$",
     accountSId: "ACbfb290aa2787537050c97adae0de5d9b",
     authToken: "0437bf7e13d4b638b30a3a35f5455e6c",
     twilioMobileNumber: "+16283000928"
