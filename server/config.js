d896bc4f1f3cb598f2b203a32a8a414503f9da52
nikhilfusion <kannanpnb@gmail.com>
2019-02-08 16:44:49 +0530

    removed auth token

--- server/config.js
+++ server/config.js
@@ -1,16 +1,16 @@
 var vid_duration = 60;
 module.exports = {
-    stream : {
-        filterTime : 5 * vid_duration
-    },
-    path: "/videos/",
-    host: "127.0.0.1",
-    port: "9080",
-    apiHost: "192.168.0.151",
-    apiPort: "8080",
-    gmailUser: "neoviewIreland@gmail.com",
-    gmailPswd: "neoview1234@#$",
-    accountSId: "ACbfb290aa2787537050c97adae0de5d9b",
-    authToken: "3a9e6cea81cd5e40264910695ac069da",
-    twilioMobileNumber: "Neoview"
+  stream: {
+    filterTime: 5 * vid_duration
+  },
+  path: '/videos/',
+  host: '127.0.0.1',
+  port: '9080',
+  apiHost: '192.168.0.151',
+  apiPort: '8080',
+  gmailUser: '',
+  gmailPswd: '',
+  accountSId: '',
+  authToken: '',
+  twilioMobileNumber: 'Neoview'
 };
